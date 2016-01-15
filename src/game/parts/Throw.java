012967dca5d43bb7d4166de5a1a0237e29f7670c
GitHhb <github@mittere.net>
2016-01-15 03:31:48 +0100

    Save work

--- src/game/parts/Throw.java
+++ src/game/parts/Throw.java
@@ -6,11 +6,25 @@ package game.parts;
 public class Throw {
     final private static int nrDices = 5; // one throw contains 5 dices
     Dice[] theDices = new Dice[nrDices];
-    boolean[] theDicesToRoll = new boolean[nrDices]; // true == roll this dice
+    private boolean[] theDicesToRoll = new boolean[nrDices]; // true == roll this dice
 
     // Init theDices
     { for (int i = 0; i<nrDices; i++)
         theDices[i] = new Dice();
+      selectAllDices();
     }
 
+    void throwSelectedDices(){
+        for (int i = 0; i<nrDices; i++)
+            if (theDicesToRoll[i]) theDices[i] =  new Dice();
+    }
+
+    void selectAllDices(){
+        for (int i = 0; i<nrDices; i++)
+            theDicesToRoll[i] = true;
+    }
+
+    void selectDice(int diceNr){
+        theDicesToRoll[diceNr] = true;
+    }
 }
